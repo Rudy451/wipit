08baedbac4a0f69fbf8b606195623107a4099a82
Rudy451 <dane.johnson451@gmail.com>
2022-05-25 15:24:06 -0700

    Heroku

--- server/models/config.ts
+++ server/models/config.ts
@@ -13,7 +13,7 @@ const dbConfig = process.env.NODE_ENV == 'development' ?
   port: 5432,
   logging: false,
 } : {
-  use_env_variable: process.env.DATABASE_URL,
+  use_env_variable: process.env.DATABASE_URL as string,
   dialect: "postgres" as Dialect,
   dialectOptions: {
     ssl: {
@@ -23,11 +23,17 @@ const dbConfig = process.env.NODE_ENV == 'development' ?
   }
 };
 
-const sequelize = new Sequelize(
+const sequelize = process.env.NODE_ENV == 'development' ?
+new Sequelize(
   DATABASE,
   USER,
   PASSWORD,
   dbConfig
-);
+) :
+new Sequelize(
+  process.env.DATABASE_URL as string, {
+  dialect: 'postgres' as Dialect,
+  protocol: 'postgres'
+})
 
 export default sequelize;
