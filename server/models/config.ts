ab67acc06799b985049d48ac18145ee3c7eb35cc
Rudy451 <dane.johnson451@gmail.com>
2022-05-25 15:16:29 -0700

    Heroku

--- server/models/config.ts
+++ server/models/config.ts
@@ -6,11 +6,21 @@ const DATABASE = process.env.LEGACYDATABASE as string;
 const USER = process.env.USER as string;
 const PASSWORD = process.env.PASSWORD as string;
 
-const dbConfig = {
+const dbConfig = process.env.NODE_ENV == 'development' ?
+{
   host: HOST,
   dialect: "postgres" as Dialect,
   port: 5432,
   logging: false,
+} : {
+  use_env_variable: process.env.DATABASE_URL,
+  dialect: "postgres" as Dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 };
 
 const sequelize = new Sequelize(
